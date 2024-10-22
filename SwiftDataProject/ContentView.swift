cd44e1e908c96073a9f520a85c698a144551d922
sxmtee <somtoo76@gmail.com>
2024-10-22 13:41:35 +0100

    SwiftDataProject done

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @State private var showingUpcominOnly = false
+    @State private var showingUpcomingOnly = false
+    
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
 //    @Query(
 //        filter: #Predicate<User> { user in
 //            if user.name.localizedStandardContains("R") {
@@ -29,13 +34,37 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            UsersView(minimumJoinDate: showingUpcominOnly ? .now : .distantPast)
+            UsersView(
+                minimumJoinDate: showingUpcomingOnly ? .now : .distantPast,
+                sortOrder: sortOrder
+            )
             .navigationTitle("Users")
             .toolbar {
                 Button("Add User", systemImage: "plus") {
 //                    let user = User(name: "", city: "", joinDate: .now)
 //                    modelContext.insert(user)
 //                    path = [user]
+                }
+                
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
         }
--- SwiftDataProject/UserView.swift
+++ SwiftDataProject/UserView.swift
@@ -9,22 +9,47 @@ import SwiftUI
 import SwiftData
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
-    init(minimumJoinDate: Date) {
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: \User.name)
     }
     
+    func addSample() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+
+        modelContext.insert(user1)
+
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
+    }
+    
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+
+                Spacer()
+
+                Text(String(user.unwrappedJobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
+        .onAppear(perform: addSample)
     }
 }
 
 #Preview {
-    UsersView()
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
         .modelContainer(for: User.self)
 }
